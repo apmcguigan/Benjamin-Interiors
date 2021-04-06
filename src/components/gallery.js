a149022a588deaa460778ccaad8df6056c324320
Adam <31741286+apmcguigan@users.noreply.github.com>
2021-04-05 21:39:35 -0300

    Insta Grid Hover Fade

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,25 +5,34 @@ const Gallery = () => (
 <div> 
   <div className="row">
     <div className="column">
-      <img src="/assets/image1.jpg"></img>
+    <a target="_blank" href="https://www.instagram.com/p/CMe3E3NJDiy/"><img src="/assets/image1.jpg"></img></a>
     </div>
     <div className="column">
-      <img src="/assets/image2.jpg"></img>
+    <a target="_blank" href="https://www.instagram.com/p/CLMsS-tpUck/">
+      <img src="/assets/image2.jpg">
+    </img></a>
     </div>
     <div className="column">
-      <img src="/assets/image3.jpg"></img>
+    <a target="_blank" href="https://www.instagram.com/p/CMNZwyvp5Qd/">
+      <img src="/assets/image3.jpg">
+      </img>
+      </a>
     </div>
   </div>
   
   <div className="row">
     <div className="column">
-      <img src="/assets/image4.jpg"></img>
+      <a target="_blank" href="https://www.instagram.com/p/CK1nF6qJKWn/"><img src="/assets/image4.jpg"></img></a>
     </div>
     <div className="column">
+      <a  target="_blank" href="https://www.instagram.com/p/CJ_fZaPFlsS/">
       <img src="/assets/image5.jpg"></img>
+      </a>
     </div>
     <div className="column">
+      <a target="_blank" href="https://www.instagram.com/p/CKKHgNnl-QV/">
       <img src="/assets/image6.jpg"></img>
+      </a>
     </div>
   </div>
 </div>
