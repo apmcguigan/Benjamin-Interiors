d0996a8744296a98c34ff261415dadbc88000053
Adam <31741286+apmcguigan@users.noreply.github.com>
2021-04-22 16:23:09 -0300

    Remove dark from index

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -12,23 +12,6 @@ export default {
     inputBackground: "#fff",
     socialIcons:"#7C7C7C",
     socialIconsHover:"#313131",
-    modes: {
-      dark: {
-        text: '#f5f5f5',
-        background: '#F0F0F0',
-        primary: '#252525',
-        accent: '#DDBEA9',
-        muted: 'rgba(255, 255, 255, 0.7)',
-        buttonAccent: '#fff',
-        cardBg: '#252525',
-        borderColor:"#888",
-        labelText: "#777",
-        inputBorder: "#777",
-        inputBackground: "#333",
-        socialIcons:"rgba(255,255,255,0.7)",
-        socialIconsHover:"#fff",
-      },
-    }
   },
   links: {
     postLink: {
